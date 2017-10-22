8bcdb456352538d6ee926a259e4e73210afa85aa
kylo666 <32401843+kylo666@users.noreply.github.com>
2017-10-22 16:44:16 +0200

    Update 9.js

--- js/9.js
+++ js/9.js
@@ -1,9 +1,12 @@
-var drawTree = 25;
+var drawTree = 25;{
+var star;
+
 for(var i=0; i<25 ; i++){
 star= '*'; 
 
-for (var j = 0; j < i; i++) { 
-star= +'*';
+for (var j = 0; j < i; j++) { 
+star+='*';
 }
 console.log(star);
+}
 }
