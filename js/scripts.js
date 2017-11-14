44fb10b624b70288adacc07e9d71744ace298515
kylo666 <32401843+kylo666@users.noreply.github.com>
2017-11-14 21:10:33 +0100

    Update and rename 9.js to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function drawTree(number) {
+	
+	for (var i = 0; i < number; i++) {
+		var star = '';	
+		for (var j = 0; j < i + 1; j++) {
+			star += '*';
+		}		
+		console.log(star);	
+	}
+}
+
+drawTree(5);
